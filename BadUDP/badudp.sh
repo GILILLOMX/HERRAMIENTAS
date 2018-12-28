f6dc6aa60028cf23bb3c18e07cb785d616ed4b09
Jars <ar851304@gmail.com>
2018-12-28 16:01:28 -0300

    Update Script

--- BadUDP/badudp.sh
+++ BadUDP/badudp.sh
@@ -15,19 +15,19 @@ kill $(ps x |grep badvpn |grep -v grep |awk '"{'"'print $1'"'})
 killall badvpn-udpgw
 fi" > /bin/badvpn
 chmod +x /bin/badvpn
-if [ -f /usr/bin/badvpn-udpgw ]; then
+if [ -f /bin/badvpn-udpgw ]; then
 echo -e "\033[1;32mBadvpn ya esta instalado\033[0m"
-rm -rf badudp.sh >/dev/null 2>/dev/null
+rm -rf $HOME/badudp.sh > /dev/null 2>&1
 exit
 else
 clear
 fi
 echo -e "\033[1;31m           Instalador Badvpn\n\033[0m"
 echo -e "Haciendo la download de Badvpn"
-wget -O /usr/bin/badvpn-udpgw https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/HERRAMIENTAS/tree/master/BadUDP/badvpn-udpgw -o /dev/null
-chmod +x /usr/bin/badvpn-udpgw
+wget -O /bin/badvpn-udpgw https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/HERRAMIENTAS/tree/master/BadUDP/badvpn-udpgw -o /dev/null
+chmod +x /bin/badvpn-udpgw
 clear
 echo -e "\033[1;32m             Instalacion completa\033[0m" 
 echo -e "\033[1;37mComandos:\n\033[1;31mbadvpn start\033[1;37m para iniciar o badvpn"
 echo -e "\033[1;31mbadvpn stop \033[1;37m para parar o badvpn\033[0m"
-rm -rf .sh >/dev/null 2>/dev/null
+rm -rf $HOME/badudp.sh > /dev/null 2>&1
